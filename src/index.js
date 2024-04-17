c18fb4236fe717d8276b6b1f5e6381de2661959c
VadymRyzhuk <ceasarj571@gmail.com>
2024-04-17 21:50:48 +0200

    update signUp/redux/schemas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor } from './redux/store';
-import { store } from './redux/store';
+import { persistor } from './reduxFolder/store';
+import { store } from './reduxFolder/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
